570f3e975d0afac25f24436345e093a4407102cf
beerose <aleksandra@edgedb.com>
2024-11-14 22:48:40 +0100

    Init

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+  root: "./src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: "./src/index.html",
+        sql: "./src/sql.html",
+        prisma: "./src/prisma.html",
+        perf: "./src/perf.html",
+        schema: "./src/schema.html",
+      },
+    },
+  },
+};
